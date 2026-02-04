ac37305782c79160620b8b12d9b345b23ff69ffe
George Ikpugbu <georgicng.2.0@gmail.com>
2026-02-04 13:10:49 +0100

    chore: Remove redundant data

--- routes/web.php
+++ routes/web.php
@@ -89,10 +89,3 @@ Route::get('/checkout/{method}',  function (OrderService $orderService, string $
 });
 Route::inertia('/success', 'Success')->name('success');
 
-
-Route::controller(PaymentController::class)->prefix('paystack')->group(function () {
-    Route::get('/redirect', 'redirect')->name('paystack.redirect');
-    Route::get('/success', 'success')->name('paystack.success');
-    Route::get('/cancel', 'cancel')->name('paystack.cancel');
-    Route::get('/pay', 'popup')->name('paystack.popup');
-});
